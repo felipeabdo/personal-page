bb63dead30cc3de86ab1ad100fabf1be001a3e36
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2024-04-21 20:30:39 -0300

    aplicativo corrigido

--- config/puma.rb
+++ config/puma.rb
@@ -17,6 +17,9 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 port ENV.fetch("PORT") { 3000 }
 
+# Define o host do servidor
+bind "tcp://0.0.0.0:#{ENV['PORT']}"
+
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
